6ee6ebcb71bf4e6b32d4fa09aea0803f994de239
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-20 11:24:15 -0600

    Clase que permite realizar funcionalidades con la tabla de Usuario

---
+++ 26 - Reservaciones/Usuario.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Agregar los namespaces de conexión con SQL Server
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace _26___Reservaciones
+{
+    class Usuario
+    {
+        // Variables miembro
+        private static string connectionString = ConfigurationManager.ConnectionStrings["_26___Reservaciones.Properties.Settings.ReservacionesConnectionString"].ConnectionString;
+        private SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        // Propiedades
+        public int Id { get; set; }
+
+        public string NombreCompleto { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Estado { get; set; }
+
+        // Constructores
+        public Usuario() { }
+
+        public Usuario(string nombreCompleto, string username, string password, bool estado)
+        {
+            NombreCompleto = nombreCompleto;
+            Username = username;
+            Password = password;
+            Estado = estado;
+        }
+
+        // Métodos
+
+        /// <summary>
+        /// Verifica si las credenciales de inicio de sesión son correctas.
+        /// </summary>
+        /// <param name="username">El nombre del usuario</param>
+        /// <returns>Los datos del usuario</returns>
+        public Usuario BuscarUsuario(string username)
+        {
+            // Crear el objeto que almacena la información de los resultados
+            Usuario usuario = new Usuario();
+
+            try
+            {
+                // Query de selección
+                string query = @"SELECT * FROM Usuarios.Usuario
+                                 WHERE username = @username";
+
+                // Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        // Obtener los valores del usuario si la consulta retorna valores
+                        usuario.Id = Convert.ToInt32(rdr["id"]);
+                        usuario.NombreCompleto = rdr["nombreCompleto"].ToString();
+                        usuario.Username = rdr["username"].ToString();
+                        usuario.Password = rdr["password"].ToString();
+                        usuario.Estado = Convert.ToBoolean(rdr["estado"]);
+                    }
+                }
+
+                // Retornar el usuario con los valores
+                return usuario;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                // Cerrar la conexión
+                sqlConnection.Close();
+            }
+        }
+    }
+}

b3fb075c293259329f2e0d4cc3eee9b058b8de4d
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-20 12:11:08 -0600

    Creación del formulario de menú principal de la aplicación

---
+++ 26 - Reservaciones/MenuPrincipal.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _26___Reservaciones
+{
+    /// <summary>
+    /// Interaction logic for MenuPrincipal.xaml
+    /// </summary>
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal(string usuario)
+        {
+            InitializeComponent();
+
+            // Mostrar el nombre en el formulario
+            lbUsuario.Content = string.Format("¡Hola {0}! ¿Qué deseas realizar?", usuario);
+        }
+
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            // Retornar el usuario al formulario de inicio de sesión
+            IniciarSesion iniciarSesion = new IniciarSesion();
+            iniciarSesion.Show();
+            Close();
+        }
+    }
+}
